ca4b36bf079151105d20b4d9a9f7a3749995d458
Luk Cogito <luk.cogito@pilsfree.net>
2024-04-06 17:15:16 +0200

    Data ready to vectorize

--- tf_idf.py
+++ tf_idf.py
@@ -28,6 +28,8 @@ def get_string_from_html(file_path):
         # Parse the text from html code
         soup = BeautifulSoup(file, 'html.parser')
         text = soup.get_text()
+    # Close the opened file
+    file.close()
     # Return the text as string var
     return text
 
@@ -47,7 +49,7 @@ def remove_tail(text):
 
 # Define a function for text preprocesing
 def preprocess_text(text):
-    # I use spaCY
+    # I use spaCy
     doc = nlp(text)
 
     # Initialize empty list for processed tokens
@@ -55,21 +57,57 @@ def preprocess_text(text):
 
     # Iterrate one each token in doc
     for token in doc:
-        # If it isn't stop word (a, and, the, etc.)
-        if not token.is_stop:
+        # If it isn't stop word (a, and, the, etc.) and it is alpha (nut a numerical value, special character, etc.)
+        if not token.is_stop and token.is_alpha:
             # Lemmatize it
             lemmatized_token = token.lemma_
-            # If it is alpha(= token with just letters without any numbers, special symbols or eveny empty spaces inc case of pronoun)
-            if token.is_alpha:
-                # If token doesn't look like number and isn't propper noun (=vlastní jméno)
-                if not token.like_num and not token.pos_ == "PROPN":
+            if (str(lemmatized_token) not in preprocessed_tokens) and (str(lemmatized_token).lower() not in preprocessed_tokens):
+                if token.pos_ == "PROPN":
                     # Add it's lematized version
-                    preprocessed_tokens.append(lemmatized_token)
+                    preprocessed_tokens.append(str(lemmatized_token))
                 else:
-                    # Add it's not lematized version
-                    preprocessed_tokens.append(token.text)
+                    # Add it's lower lemmatized version
+                    preprocessed_tokens.append(str(lemmatized_token).lower())
 
     return preprocessed_tokens
+
+# Define a funciton for extracting querry text and number from xml file
+def get_data_from_xml(file_path):
+    # Read the xml file
+    with open('query_devel.xml', 'r') as file:
+        xml_data = file.read()
+        # Initialize list for xml docs
+        xml_docs = []
+
+        # Prepare re pattern for extraction of each <DOC>content</DOC> in file
+        pattern = re.compile(r'<DOC>(.*?)</DOC>', re.DOTALL)
+        matches = pattern.finditer(xml_data)
+
+        # Extract every single query
+        for match in matches:
+            xml_doc = match.group(0).strip()
+            xml_docs.append(xml_doc)
+
+        # Prepare dic for preprocessed queries
+        queries = {}
+
+        # Iterrate thorugh the non-processed docs
+        for doc in xml_docs:
+            # Make soup out of it
+            soup = BeautifulSoup(doc, "xml")
+            # Extract query number
+            doc_number = soup.find("DOCNO").text
+            doc_number = int(doc_number)
+            # Extract query text
+            doc_text = soup.find("DOC").text.strip()
+            # Make tokens out of the text
+            doc_tokens = preprocess_text(doc_text)
+            # And fill the dict
+            queries[doc_number] = doc_tokens
+    # Cloese the file
+    file.close()
+    # And return result dict with queries
+    return queries
 
 
 # Initilaize dictionary for storing doc id as a key and doc keywoards list as a value
@@ -98,3 +136,7 @@ for filename in os.listdir(directory):
         documents[doc_id] = tokens
 
 print(documents)
+
+path = "./query_devel.xml"
+queries = get_data_from_xml(path)
+print(queries)
